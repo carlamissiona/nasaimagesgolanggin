82c4b2758be9f11693f82cc71826b340f6c5db2d
missiona.carla <missiona.carla@gmail.com>
2022-04-28 21:15:12 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,45 +8,28 @@ import (
 
 var Router * gin.Engine
 
-func showHome(c *gin.Context) {
-	 
-
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		// Set the HTTP status to 200 (OK)
-		http.StatusOK,
-		// Use the index.html template
-		"index.html",
-		// Pass the data that the page uses
-		gin.H{
-			"title":   "Home Page",
-			"payload": "hi",
-		},
-	)
-
-}
-
-func showEachItem(c *gin.Context) {
-}
-
-
-
-
 
 
 func main() {
+	
 
 	r := gin.Default()
-	Router.LoadHTMLGlob("templates/*")
+	r.LoadHTMLGlob("templates/*")
 
-	Router.GET("/updatesemail", func(c *gin.Context) {
+	r.GET("/updatesemail", func(c *gin.Context) {
 		sendemail("codetuna@protonmail.com")
 		c.JSON(200, gin.H{
 			"message": "Hello world!",
 		})
 	})
 	
-	Router.GET("/", showHome)
+	r.GET("/app", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+			"payload": "hi"
+		})
+	})
+	 
 	r.Run()
 
  
