2b27ad253779371c2ba7282a48ec5926a99d116a
missiona.carla <missiona.carla@gmail.com>
2022-04-28 20:06:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"net/http"
+	"strconv"
 	"github.com/gin-gonic/gin"
 )
+
 var Router * gin.Engine
+
+func showHome(c *gin.Context) {
+	 
+
+	// Call the HTML method of the Context to render a template
+	c.HTML(
+		// Set the HTTP status to 200 (OK)
+		http.StatusOK,
+		// Use the index.html template
+		"index.html",
+		// Pass the data that the page uses
+		gin.H{
+			"title":   "Home Page",
+			"payload": "hi",
+		},
+	)
+
+}
+
+func showEachItem(c *gin.Context) {
+}
+
+
+
+
+
+
 func main() {
+
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello world!",
@@ -17,5 +50,17 @@ func main() {
 			"message": "Hello world!",
 		})
 	})
+	
+	r.GET("/", showHome)
 	r.Run()
+
+ 
+
+
+	// Handle the index route
+	
+	//router.GET("/images/view/:id", showEachItem)
+	
+
+	// Start serving the application
 }
